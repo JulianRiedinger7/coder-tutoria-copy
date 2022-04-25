8060fd2babae98f2ff1c8b4325bba644234db997
Julian Riedinger <julianriedinger27@gmail.com>
2022-04-25 15:06:11 -0300

    Primer version only mobile

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'bg-primary': '#FCF7F0',
+        'btn-primary': '#8443D8'
+      }
+    },
+  },
+  plugins: [],
+}
